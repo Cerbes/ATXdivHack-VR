741061b216e569911c784472596ee9a34c95eddb
Cerbes <Courtney@Courtneys-MacBook-Air.local>
2017-09-17 09:18:57 -0500

    Removed text; changed light source

--- client/src/components/VR/index.js
+++ client/src/components/VR/index.js
@@ -18,9 +18,8 @@ export default class VR extends React.Component {
 
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
         <Entity primitive="a-light" type="ambient" color="#445451"/>
-        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
+        <Entity primitive="a-light" type="point" intensity="3" position="0 10 0"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
         <Entity obj-model="obj: #spider-obj;" material="color: #000000" position={{x: 0, y: 0, z: -3}} scale={{ x: 20, y: 20, z: 20 }}/>
       </Scene>
     );
