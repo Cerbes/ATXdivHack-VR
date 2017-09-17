dcbb12edf1a79ba9130d217f7e4bfb3d77370086
Cerbes <Courtney@Courtneys-MacBook-Air.local>
2017-09-17 11:58:31 -0500

    merged chase/VR-8

---
+++ client/src/components/VR/spider.js
@@ -0,0 +1,16 @@
+import 'aframe';
+import 'aframe-animation-component';
+import { Entity, Scene } from 'aframe-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Spider extends React.Component {
+  constructor() {
+    super();
+  }
+  render () {
+    return (
+        <Entity obj-model="obj: #spider-obj;" material="color: red;" position={{x: 0, y: 0, z: 0}} scale={{ x: 15, y: 15, z: 15 }} animation__scale="property: position; dir: alternate; dur: 1000; loop: true; to: 5 0 5"/>
+    );
+  }
+}
