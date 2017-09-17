60d25442c491cc38a8cd5b814275efca1e7b5911
Stephen Mayeux <stephenmayeux@gmail.com>
2017-09-17 10:18:45 -0500

    merge vr-7

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from '../../actions'
 
-export default class App extends Component {
+class App extends Component {
   render() {
+    const { fears, actions } = this.props
+    const childrenWithProps = React.cloneElement(this.props.children, { fears, actions })
     return (
       <div>
-        {this.props.children}
+        {childrenWithProps}
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ fears }) => {
+  return { fears }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return { actions: bindActionCreators(actionCreators, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
