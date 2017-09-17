426f2814c75585d4153fa8119a60e9aaac22e170
Cerbes <Courtney@Courtneys-MacBook-Air.local>
2017-09-17 11:57:43 -0500

    Added spider animation

--- client/src/components/VR/index.js
+++ client/src/components/VR/index.js
@@ -1,7 +1,9 @@
 import 'aframe';
+import 'aframe-animation-component';
 import { Entity, Scene } from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Spider from "./spider.js"
 
 const API_SERVER = 'http://localhost:3000'
 
@@ -19,8 +21,9 @@ export default class VR extends React.Component {
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
         <Entity primitive="a-light" type="ambient" color="#445451"/>
         <Entity primitive="a-light" type="point" intensity="3" position="0 10 0"/>
+        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity obj-model="obj: #spider-obj;" material="color: #000000" position={{x: 0, y: 0, z: -3}} scale={{ x: 20, y: 20, z: 20 }}/>
+	<Spider/>
       </Scene>
     );
   }
